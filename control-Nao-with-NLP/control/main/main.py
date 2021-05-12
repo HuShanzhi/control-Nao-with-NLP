25919709f18625018bf3e088473485fa79ba0ea9
Hu-Xiaoming <uhanhi@163.com>
2021-05-12 14:46:18 +0800

    调整

--- control-Nao-with-NLP/control/main/main.py
+++ control-Nao-with-NLP/control/main/main.py
@@ -2,7 +2,7 @@
 # Author: HuXiaoMing
 # Date: 2021-4-22
 # 主函数
-# 如果提示找不到_controller包，是因为main.py文件的Environments Variables未配置
+# 如果提示ImportError：“找不到_controller包”，是因为main.py文件的Environments Variables未配置
 from control.controller.naoController import Nao
 
 
